efa8942abcc2cd60c2f181362ecceda9f0a3bfef
Yu YAGIHASHI <yagihashoooooo@gmail.com>
2018-11-28 20:59:29 +0900

    Fix flow to input weight

--- index.js
+++ index.js
@@ -35,13 +35,11 @@ const updateWeight = async (weight) => {
     await page.type('#signin > div > div.col-xs-12.col-sm-6.col-md-4.col-lg-3.sidebar > div.contentForm > form > div:nth-child(2) > input', password)
     await page.click('#signin > div > div.col-xs-12.col-sm-6.col-md-4.col-lg-3.sidebar > div.contentForm > form > div.createButton > button')
 
-    // Open weight history page
-    await page.waitFor('#timeline-summary-sticky > div.summary-list > div > div:nth-child(1) > div.details')
-    await page.click('#timeline-summary-sticky > div.summary-list > div > div:nth-child(1) > div.details')
-
     // Open dialog to input weight log
-    await page.waitFor('#weight-content > div > div.addbutton')
-    await page.click('#weight-content > div > div.addbutton')
+    await page.waitFor('#timeline-content > div > div.addbutton.menu > div:nth-child(1)')
+    await page.click('#timeline-content > div > div.addbutton.menu > div:nth-child(1)')
+    await page.waitFor('#timeline-content > div > div.addbutton.menu.active > ul > li:nth-child(5) > div.icon')
+    await page.click('#timeline-content > div > div.addbutton.menu.active > ul > li:nth-child(5) > div.icon')
 
     // Input weight and save
     await page.waitFor('#weight-add > form > div.weight-and-mass > div:nth-child(1) > div.value.focus-weight > input')
